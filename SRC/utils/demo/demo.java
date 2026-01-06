d74267145135c004003f76206d0c6f5fb14c17a9
LinhNgo14 <linhnt.dev@scuti.asia>
2026-01-06 15:36:01 +0700

    Update demo.java

--- SRC/utils/demo/demo.java
+++ SRC/utils/demo/demo.java
@@ -14,7 +14,7 @@ public class StringTools {
         return text.length();
     }
 
-    // Ví dụ dùng
+    // Ví dụ dùng hhheeehehehhehehheeh
     public static void main(String[] args) {
         StringTools tool = new StringTools();
         String s = "hello world";
